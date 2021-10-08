4333d7cd7fe65189519394a8e40e6faba8347772
Hoang Nguyen <nguyenduchoang12c5@gmail.com>
2021-10-08 11:57:42 +0700

    update

--- app/Http/Controllers/Api/v1/MusicsController.php
+++ app/Http/Controllers/Api/v1/MusicsController.php
@@ -18,7 +18,12 @@ class MusicsController extends Controller
      */
     public function index()
     {
-        var_dump(DB::connection()->getPdo());die;
+        try {
+            DB::connection()->getPdo();
+        } catch (\Exception $e) {
+            var_dump($e);die;
+            die("Could not connect to the database.  Please check your configuration. error:" . $e );
+        }
         // $music = new CustomerCollection(Music::paginate(5));
         // return $music;
     }
