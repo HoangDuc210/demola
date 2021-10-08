eb3ffe21f99ea79e133258c8a9c48e8f11844ca8
Hoang Nguyen <nguyenduchoang12c5@gmail.com>
2021-10-08 09:30:40 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,13 +8,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel & React</title>
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="https://demola-back.herokuapp.com/css/app.css" rel="stylesheet">
 </head>
 <body>
 <div id="app">
 
 </div>
 
-<script src="{{ asset('js/app.js') }}"></script>
+<!-- <script src="{{ asset('js/app.js') }}"></script> -->
+<script src="https://demola-back.herokuapp.com/js/app.js"></script>
 </body>
 </html>
